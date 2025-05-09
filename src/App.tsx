fc34ff0a87263b74c54a448c10b06f19bca54654
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2025-05-09 14:16:04 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold">What tech stack we use?</h1>
+        <ul className="text-2xl font-medium">
+          <li>React.TS</li>
+          <li>Tailwind CSS</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
